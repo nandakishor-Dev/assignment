825e048c8f2bb756d92a1892a21efdfea630b71e
Chaavie Nandakishor <chaavienandakishor@gmail.com>
2024-03-20 14:27:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { appRoutes } from "./routes"
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {appRoutes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={route.element}
+          />
+        ))}
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/routes.jsx
@@ -0,0 +1,3 @@
+import { Signin } from "./pages/Signin/Signin";
+
+export const appRoutes = [{ path: "/", element: <Signin /> }];
