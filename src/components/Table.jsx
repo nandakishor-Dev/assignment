f8d9cf57366775e0e2c57d9b4b44dcdf2dcaae98
Chaavie Nandakishor <chaavienandakishor@gmail.com>
2024-03-23 10:33:43 +0530

    refactor: added select tags,removed tags

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,8 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import React from "react";
 
-export const Table = ({ tableData, showData }) => {
+export const Table = ({ tableData, showData, onSelectTag, handleRemoveTag }) => {
+
   return (
     <>
       {showData && (
@@ -56,7 +58,9 @@ export const Table = ({ tableData, showData }) => {
                   </div>
                   <div className="w-1/5">
                     <div className="text-sm font-figtree font-normal pl-3">
+
                       <select
+                        onChange={(event) => onSelectTag(index, event.target.value)}
                         className="p-2 border border-gray-200 rounded-lg"
                         name=""
                         id=""
@@ -75,12 +79,38 @@ export const Table = ({ tableData, showData }) => {
                           </option>
                         ))}
                       </select>
+                      {/* <FormControl fullWidth sx={{ marginTop: 0, marginBottom: 0 }}>
+
+                   
+                        <Select
+                          labelId="demo-simple-select-label"
+                          id="demo-simple-select"
+                          size="small"
+                          className="p-2 border border-gray-200 rounded-lg"
+                          sx={{ borderRadius: '5px', width: "80%" }}
+                          label="Age"
+                          onChange={(event) => onSelectTag(index, event.target.value)}
+                        >
+
+                          {row.selectTags.map((tag, tagIndex) => (
+                            <MenuItem key={tagIndex} value={tag}>      {tag}</MenuItem>
+                          ))}
+
+                        </Select>
+                      </FormControl> */}
+
                     </div>
                   </div>
-                  <div className="w-2/6 text-sm font-figtree font-normal pl-3 ">
-                    <div className="bg-blue-500 rounded-md p-1 h-6 w-16 text-white text-xs flex justify-center items-center">
-                      Tag 1
-                    </div>
+                  <div className="w-2/6 text-sm font-figtree flex gap-3 font-normal pl-3 flex-wrap ">
+                    {row.selectedTags.map((selectedTags, selectedTagIndex) => (
+
+                      <div className="bg-blue-500 rounded-md p-2 h-6 w-auto text-white text-xs flex justify-center items-center gap-2">
+                        <span>{selectedTags}</span>
+                        <span className="cursor-pointer "
+                          onClick={() => handleRemoveTag(index,selectedTagIndex)}
+                        >X</span>
+                      </div>
+                    ))}
                   </div>
                 </div>
               ))}
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -16,145 +16,163 @@ import excel from "../../assets/images/excel.png";
 import papaparse from "papaparse";
 
 export const Dashboard = () => {
-  const [csvData, setCsvData] = useState();
-  const [showData, setShowData] = useState(false);
-  const [fileName, setFileName] = useState("");
+    const [csvData, setCsvData] = useState();
+    const [showData, setShowData] = useState(false);
+    const [fileName, setFileName] = useState("");
 
-  const handleFileSelect = (e) => {
-    const fileList = e.target.files;
-    const newFiles = Array.from(fileList);
-    const file = newFiles[0].name;
-    setFileName(file);
-    setShowData(false);
+    const handleFileSelect = (e) => {
+        const fileList = e.target.files;
+        const newFiles = Array.from(fileList);
+        const file = newFiles[0].name;
+        setFileName(file);
+        setShowData(false);
 
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const csvContent = event.target.result;
-      papaparse.parse(csvContent, {
-        header: true,
-        complete: function (results) {
-          const data = results.data;
-          const cleanedData = data.map((ele) => {
-            let selectTags = ele["select tags"].split(",");
-            delete ele["select tags"];
-            delete ele["selected tags"]
-            return {
-              ...ele,
-              selectTags,
-              selectedTags:[]
-            };
-          });
-          setCsvData(cleanedData);
-        },
-      });
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            const csvContent = event.target.result;
+            papaparse.parse(csvContent, {
+                header: true,
+                complete: function (results) {
+                    const data = results.data;
+                    const cleanedData = data.map((ele) => {
+                        let selectTags = ele["select tags"].split(",");
+                        delete ele["select tags"];
+                        delete ele["selected tags"]
+                        return {
+                            ...ele,
+                            selectTags,
+                            selectedTags: []
+                        };
+                    });
+                    setCsvData(cleanedData);
+                },
+            });
+        };
+
+        reader.readAsText(e.target.files[0]);
     };
 
-    reader.readAsText(e.target.files[0]);
-  };
+    //   console.log("file",file)
 
-  //   console.log("file",file)
+    const uploadData = () => {
+        if (fileName && csvData.length > 0) {
+            setShowData(true);
+        }
+    };
 
-  const uploadData = () => {
-    if (fileName && csvData.length > 0) {
-      setShowData(true);
+    const handleRemove = () => {
+        setFileName("");
+        setCsvData();
+        setShowData(false);
+    };
+    //updating tags
+    const handleSelectTag = (index, value) => {
+        const clonedCsvData = [...csvData]
+        const { selectedTags } = clonedCsvData[index]
+        const updatedSelectedTags = [...selectedTags, value]
+        clonedCsvData[index].selectedTags = updatedSelectedTags
+        setCsvData(clonedCsvData)
     }
-  };
+    //remove tags
+    const handleRemoveTag = (index,tagIndex) => {
+        const clonedCsvData = [...csvData]
+        let { selectedTags } = clonedCsvData[index]
+        selectedTags.splice(tagIndex, 1)
+        const updatedSelectedTags = [...selectedTags]
+        clonedCsvData[index].selectedTags = updatedSelectedTags
+        setCsvData(clonedCsvData)
+    }
 
-  const handleRemove = () => {
-    setFileName("");
-    setCsvData();
-    setShowData(false);
-  };
 
-  console.log({ csvData });
-  return (
-    <div className="flex">
-      <div className="bg-white w-1/6">
-        <div className="h-28 flex items-center  justify-center gap-4 ">
-          <img src={logo} alt="" />
-          <span className="font-montserrat font-normal text-2xl text-black">
-            Base
-          </span>
-        </div>
-        <div className="pt-4 flex flex-col gap-8 ">
-          <SidebarOption text="Dashboard" image={dashboard} />
-          <SidebarOption text="Upload" image={upload} />
-          <SidebarOption text="Invoice" image={ticket} />
-          <SidebarOption text="Schedule" image={Document} />
-          <SidebarOption text="Calender" image={calendar} />
-          <SidebarOption text="Notification" image={notification} />
-          <SidebarOption text="Settings" image={setting} />
-        </div>
-      </div>
-      <div className="bg-custom  w-full pl-4 pr-4 pt-8">
-        <div className="h-18 flex justify-between ">
-          <div>
-            <span className="font-figtree font-semibold text-2xl">
-              Upload CSV
-            </span>
-          </div>
-          <div className="flex gap-6 justify-center align-middle">
-            <div className="flex  justify-center align-middle">
-              <img className="w-6 h-8" src={noti} alt="" />
-            </div>
-            <div className="flex  justify-center align-middle">
-              <img className="rounded-2xl" src={profile} alt="" />
-            </div>
-          </div>
-        </div>
-        <div className=" min-h-screen flex flex-col">
-          <div className="h-96  flex  justify-center ">
-            <div className="rounded-lg bg-white h-4/5 w-2/5 flex flex-col gap-5 p-3 mt-14">
-              <div className="border border-dotted rounded-lg h-4/5 flex items-center justify-center flex-col gap-5">
-                <div>
-                  <img src={excel} alt="" />
+    // console.log({ csvData });
+    return (
+        <div className="flex">
+            <div className="bg-white w-1/6">
+                <div className="h-28 flex items-center  justify-center gap-4 ">
+                    <img src={logo} alt="" />
+                    <span className="font-montserrat font-normal text-2xl text-black">
+                        Base
+                    </span>
                 </div>
-                <div>
-                  {fileName ? (
-                    fileName
-                  ) : (
-                    <>
-                      Drop your excel sheet here or
-                      <label
-                        htmlFor="fileInput"
-                        className="text-blue-500 cursor-pointer"
-                      >
-                        {" "}
-                        browse
-                      </label>
-                      <input
-                        type="file"
-                        id="fileInput"
-                        style={{ display: "none" }}
-                        accept=".csv"
-                        onChange={handleFileSelect}
-                      />
-                    </>
-                  )}
+                <div className="pt-4 flex flex-col gap-8 ">
+                    <SidebarOption text="Dashboard" image={dashboard} />
+                    <SidebarOption text="Upload" image={upload} />
+                    <SidebarOption text="Invoice" image={ticket} />
+                    <SidebarOption text="Schedule" image={Document} />
+                    <SidebarOption text="Calender" image={calendar} />
+                    <SidebarOption text="Notification" image={notification} />
+                    <SidebarOption text="Settings" image={setting} />
                 </div>
-                {fileName && (
-                  <div
-                    onClick={handleRemove}
-                    className="text-red-500 cursor-pointer"
-                  >
-                    Remove
-                  </div>
-                )}
-              </div>
-              <div className="h-1/5 ">
-                <Button handleClick={uploadData} text="Upload" />
-              </div>
             </div>
-          </div>
+            <div className="bg-custom  w-full pl-4 pr-4 pt-8">
+                <div className="h-18 flex justify-between ">
+                    <div>
+                        <span className="font-figtree font-semibold text-2xl">
+                            Upload CSV
+                        </span>
+                    </div>
+                    <div className="flex gap-6 justify-center align-middle">
+                        <div className="flex  justify-center align-middle">
+                            <img className="w-6 h-8" src={noti} alt="" />
+                        </div>
+                        <div className="flex  justify-center align-middle">
+                            <img className="rounded-2xl" src={profile} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className=" min-h-screen flex flex-col">
+                    <div className="h-96  flex  justify-center ">
+                        <div className="rounded-lg bg-white h-4/5 w-2/5 flex flex-col gap-5 p-3 mt-14">
+                            <div className="border border-dotted rounded-lg h-4/5 flex items-center justify-center flex-col gap-5">
+                                <div>
+                                    <img src={excel} alt="" />
+                                </div>
+                                <div>
+                                    {fileName ? (
+                                        fileName
+                                    ) : (
+                                        <>
+                                            Drop your excel sheet here or
+                                            <label
+                                                htmlFor="fileInput"
+                                                className="text-blue-500 cursor-pointer"
+                                            >
+                                                {" "}
+                                                browse
+                                            </label>
+                                            <input
+                                                type="file"
+                                                id="fileInput"
+                                                style={{ display: "none" }}
+                                                accept=".csv"
+                                                onChange={handleFileSelect}
+                                            />
+                                        </>
+                                    )}
+                                </div>
+                                {fileName && (
+                                    <div
+                                        onClick={handleRemove}
+                                        className="text-red-500 cursor-pointer"
+                                    >
+                                        Remove
+                                    </div>
+                                )}
+                            </div>
+                            <div className="h-1/5 ">
+                                <Button handleClick={uploadData} text="Upload" />
+                            </div>
+                        </div>
+                    </div>
 
-          <div className=" min-h-screen">
-            <p className="font-figtree font-semibold text-2xl">Uploads</p>
-            <div className="flex items-center h-full justify-center  pt-5">
-              <Table tableData={csvData} showData={showData} />
+                    <div className=" min-h-screen">
+                        <p className="font-figtree font-semibold text-2xl">Uploads</p>
+                        <div className="flex items-center h-full justify-center  pt-5">
+                            <Table tableData={csvData} showData={showData} onSelectTag={handleSelectTag} handleRemoveTag={handleRemoveTag} />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
